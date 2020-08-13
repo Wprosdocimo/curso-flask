1b90aa18aee18d07a324a9d541649f0c196fa1c9
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-08-13 07:49:00 -0300

    Atualizações da aula 10

--- delivery-wp/delivery/ext/auth/commands.py
+++ delivery-wp/delivery/ext/auth/commands.py
@@ -1,9 +1,10 @@
 import click
 
 from delivery.ext.auth.models import User
+from delivery.ext.auth.controller import create_user
 from delivery.ext.db import db
 
-
+# TODO: Mover para controler
 def list_users():
     users = User.query.all()
     click.echo(f"lista de usuarios {users}")
@@ -14,7 +15,6 @@ def list_users():
 @click.option("--admin", "-a", is_flag=True, default=False)
 def add_user(email, passwd, admin):
     """adiciona novo usuario"""
-    user = User(email=email, passwd=passwd, admin=admin)
-    db.session.add(user)
-    db.session.commit()
+    # TODO: tratar User Exist Exceptions
+    user = create_user(email=email, password=passwd, admin=admin)
     click.echo(f"Usuário {email} criado com sucesso!")
